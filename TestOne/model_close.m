6ea41aeaed5117b150e47835e1930921ddd836fa
Annaxn <xiaonj2@uw.edu>
2026-03-06 16:13:42 -0800

    Add TestOne project files

---
+++ TestOne/model_close.m
@@ -0,0 +1,13 @@
+function model_close(model)
+%This function runs when the model is closed.  It will remove the model
+%and its support folders from the path.
+
+mdlPath = fileparts(which(bdroot));
+paths = textscan(path, '%s', 'Delimiter', pathsep);
+paths = paths{1};
+for p = 1:length(paths)
+    if(strfind(paths{p}, mdlPath) == 1)
+        rmpath(paths{p});
+    end
+end
+clear mdlPath;
